9eebc870ee7834241e6f648391764437c44bf47d
thiaguinhosolucoesia-tech <thiaguinhosolucoesia@gmail.com>
2025-10-27 20:22:57 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,24 @@
+// =================================================================
+// ARQUIVO DE CONFIGURAÇÃO (V4 - Rede Social)
+// PREENCHA OS CAMPOS ABAIXO COM SUAS CHAVES
+// =================================================================
+
+// 1. COLE AQUI SEU OBJETO DE CONFIGURAÇÃO DO FIREBASE
+// (Configurações do Projeto -> Seus apps -> SDK de configuração)
+const firebaseConfig = {
+    apiKey: "AIzaSyDJ5E0OeVZyyegT1dPLhk7WN9qSsmSSlrE",
+    authDomain: "rede-social-corridas.firebaseapp.com",
+    databaseURL: "https://rede-social-corridas-default-rtdb.firebaseio.com",
+    projectId: "rede-social-corridas",
+    storageBucket: "rede-social-corridas.firebasestorage.app",
+    messagingSenderId: "189038379811",
+    appId: "1:189038379811:web:b92f89e34b246b4547c296"
+  };
+
+// 2. COLE AQUI SEU CLOUD NAME DO CLOUDINARY
+// (Dashboard -> Cloud Name)
+const CLOUDINARY_CLOUD_NAME = "dpaayfwlj";
+
+// 3. COLE AQUI SEU UPLOAD PRESET DO CLOUDINARY
+// (Settings -> Upload -> Upload Presets -> Nome do seu preset "Unsigned")
+const CLOUDINARY_UPLOAD_PRESET = "rede_corridas_unsigned";
